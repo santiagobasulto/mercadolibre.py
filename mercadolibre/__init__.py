2c832d8f562217bde49813fd72998f24270496b6
Martin Zugnoni <martin.zugnoni@gmail.com>
2014-03-13 20:00:16 -0300

    Project structure initialization

---
+++ mercadolibre/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+mercadolibre.py
+===============
+
+See http://mercadolibre-py.readthedocs.org/ for documentation.
+
+"""
+
+__title__ = 'mercadolibre.py'
+__author__ = 'Martin Zugnoni'
+__license__ = ''
+__copyright__ = ''
+__version__ = '0.0.1'
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
+
+from .client import MercadoLibre
---
+++ mercadolibre/client.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class MercadoLibre:
+
+    def __init__(self):
+        pass
