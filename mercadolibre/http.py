e04be4d033a5de346a38f7a7df616c2a7b43881a
Santiago Basulto <santiago.basulto@gmail.com>
2014-08-27 09:33:21 -0300

    Fixing SSLv3 support for ML broken SSL setup

--- mercadolibre/http.py
+++ mercadolibre/http.py
@@ -1,4 +1,8 @@
+import ssl
+
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
 
 from . import config
 
@@ -10,6 +14,20 @@ def default_headers():
         'Content-type': config.JSON_MIME_TYPE,
         'Accept-Charset': config.CHARACTER_SET,
     }
+
+
+class Ssl3HttpAdapter(HTTPAdapter):
+    """"Transport adapter" that allows us to use SSLv3.
+    Originally derived from requests docs, this allows us to overcome
+    MercadoLibre limitations with a poorly implemented SSL Setup:
+
+    https://www.ssllabs.com/ssltest/analyze.html?d=api.mercadolibre.com
+    """
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_SSLv3)
 
 
 class MercadoLibreSession(requests.Session):
@@ -26,7 +44,11 @@ class MercadoLibreSession(requests.Session):
             params.update({'access_token': self.credentials.access_token})
             kwargs['params'] = params
 
-        return super(MercadoLibreSession, self).request(*args, **kwargs)
+        self.mount(config.API_ROOT, Ssl3HttpAdapter())
+
+        response = super(MercadoLibreSession, self).request(*args, **kwargs)
+
+        return response
 
 
 def get_session(credentials):
