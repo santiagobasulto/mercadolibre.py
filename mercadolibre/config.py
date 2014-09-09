41fb608d3c8f4bf85604cc617c4545a77fc56a2e
Santiago Basulto <santiago.basulto@gmail.com>
2014-09-09 18:20:40 -0300

    Removed version from user agent

--- mercadolibre/config.py
+++ mercadolibre/config.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from . import __version__
-
 
 API_ROOT = 'https://api.mercadolibre.com'
 AUTH_URL = 'https://auth.mercadolibre.com/authorization'
 OAUTH_URL = '{0}{1}'.format(API_ROOT, '/oauth/token')
-USER_AGENT = 'mercadolibre.py-v{version}'.format(version=__version__)
+USER_AGENT = 'mercadolibre.py'
 JSON_MIME_TYPE = 'application/json'
 CHARACTER_SET = 'utf-8'
